d89ff30a4c236db29a524b76387d93a7976c6593
tsemayess <tsemaye.oseragbaje@smoothstack.com>
2021-03-12 13:04:27 -0500

    Test Suite for Assign 3

---
+++ WeekOne/src/com/smoothstack/dayfive/assignmentthree/TestSuite.java
@@ -0,0 +1,11 @@
+package com.smoothstack.dayfive.assignmentthree;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({BufferTest.class, ConsumerTest.class, ProducerTest.class})
+public class TestSuite {
+
+}
